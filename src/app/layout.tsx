c9da40bd027c010e18d07831877850ceefbfee0d
Franck <kabasele467@gmail.com>
2025-06-19 06:25:29 -0400

    Update Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./Footer";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
